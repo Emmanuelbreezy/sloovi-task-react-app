e9155b7a457f29de56d6a2f3fb9270be016d3952
Emmanuel Umeh <35114137+Emmanuelbreezy@users.noreply.github.com>
2022-06-22 07:30:59 +0100

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,3 @@
 
-export const REDUX_PERSIST_SECRET="1b9d6bef-bbfd-4b2d-9b5d" //process.env.REACT_APP_REDUX_PERSIST_SECRET;
-export const API_BASE_URL = 'https://stage.api.sloovi.com';
+export const REDUX_PERSIST_SECRET="1b9d6bef-bbfd-4b2d-9b5d"; //process.env.REACT_APP_REDUX_PERSIST_SECRET
+export const API_BASE_URL = 'https://stage.api.sloovi.com';  //process.env.API_BASE_URL
